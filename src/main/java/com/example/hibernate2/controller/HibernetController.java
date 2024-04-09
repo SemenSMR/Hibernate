5a2ccd51a1e6fc667c67136d1338be76f479baad
unknown <svorovali@mail.ru>
2024-04-09 14:44:55 +0300

    init commit

--- src/main/java/com/example/hibernate2/controller/HibernetController.java
+++ src/main/java/com/example/hibernate2/controller/HibernetController.java
@@ -4,7 +4,10 @@ import com.example.hibernate2.HibernateRepository;
 import com.example.hibernate2.OrdersRepository;
 import com.example.hibernate2.PersonRepository;
 import com.example.hibernate2.entity.Persons;
+import jakarta.annotation.security.RolesAllowed;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +24,7 @@ public class HibernetController {
 
 
     @GetMapping("/persons/by-city")
-
+    @Secured("ROLE_READ")
     public List<Persons> getPersons(@RequestParam String city) {
         return personRepository.findByCityOfLiving(city);
 
@@ -29,12 +32,14 @@ public class HibernetController {
     }
 
     @GetMapping("/products/fetch-product")
+    @PreAuthorize("hasRole('ROLE_WRITE') or hasRole('ROLE_DELETE')")
     public List<String> featchProduct(@RequestParam String name) {
         return ordersRepository.findByProductNameByCustomerNameIgnoreCase(name);
     }
 
 
     @GetMapping("/persons/age")
+    @PreAuthorize("authentication.principal.username == 'admin'")
     public List<Persons> getPersonsAge(@RequestParam int age) {
         return personRepository.findByAgeLessThanOrderByAgeAsc(age);
 
@@ -42,7 +47,7 @@ public class HibernetController {
     }
 
     @GetMapping("/persons/nameandsurname")
-
+    @Secured("ROLE_READ")
     public List<Persons> getPersonsNameAndSurname(@RequestParam String name, String surname) {
         return personRepository.findByNameAndSurname(name, surname);
 
--- src/main/java/com/example/hibernate2/security/SecurityConfig.java
+++ src/main/java/com/example/hibernate2/security/SecurityConfig.java
@@ -3,21 +3,56 @@ package com.example.hibernate2.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true, prePostEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/persons/by-city").permitAll()
+             //           .requestMatchers("/persons/by-city").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails reader = User.builder()
+                .username("reader")
+                .password(passwordEncoder().encode("reader"))
+                .roles("READ")
+                .build();
+        UserDetails writer = User.builder()
+                .username("writer")
+                .password(passwordEncoder().encode("writer"))
+                .roles("WRITE")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("READ", "WRITE", "DELETE")
+                .build();
+        return new InMemoryUserDetailsManager(admin, reader, writer);
     }
 }
